b97e57bb4ebf39f487e2de57d45e65f22b860998
Vach <vache.vardanyan.vach@gmail.com>
2025-11-22 19:14:57 +0400

    Implementing Store

--- src/components/Dock.jsx
+++ src/components/Dock.jsx
@@ -3,8 +3,13 @@ import {Tooltip} from "react-tooltip";
 import {gsap} from "gsap";
 import {dockApps} from "#constants/index.js";
 import {useGSAP} from "@gsap/react";
+import useWindowStore from "#store/window.js";
 
 const Dock = () => {
+    const { openWindow, closeWindow, windows } = useWindowStore();
+
+
+
     const dockRef = useRef(null);
 
     useGSAP(() => {
@@ -49,7 +54,22 @@ const Dock = () => {
 
 
     const toggleApp = (app) => {
+        if(!app.canOpen) return;
 
+        const window = windows[app.id];
+
+        if(!window) {
+            console.error(`Window not found for app: ${app.id}`);
+            return;
+        }
+
+        if(window.isOpen){
+            closeWindow(app.id);
+        } else {
+            openWindow(app.id);
+        }
+
+        console.log(windows);
     }
 
     return (
