81e959344bb53cf09ac956ebaf72480b992ee067
Vach <vache.vardanyan.vach@gmail.com>
2025-11-22 15:58:36 +0400

    First Commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import NavBar from "#components/NavBar.jsx";
+
+function App() {
+    return (
+        <main>
+            <NavBar />
+        </main>
+
+    )
+}
+
+export default App
