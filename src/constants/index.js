a2b90ff4e9243429c747cc38b03d05ffdd708aad
Vach <vache.vardanyan.vach@gmail.com>
2025-11-23 16:19:28 +0400

    Implementing Terminal

--- src/constants/index.js
+++ src/constants/index.js
@@ -102,27 +102,19 @@ const blogPosts = [
 const techStack = [
     {
         category: "Frontend",
-        items: ["React.js", "Next.js", "TypeScript"],
-    },
-    {
-        category: "Mobile",
-        items: ["React Native", "Expo"],
+        items: ["React.js", "Next.js"],
     },
     {
         category: "Styling",
         items: ["Tailwind CSS", "Sass", "CSS"],
     },
     {
-        category: "Backend",
-        items: ["Node.js", "Express", "NestJS", "Hono"],
-    },
-    {
-        category: "Database",
-        items: ["MongoDB", "PostgreSQL"],
-    },
-    {
         category: "Dev Tools",
-        items: ["Git", "GitHub", "Docker"],
+        items: ["Git", "GitHub", "MongoDB"],
+    },
+    {
+        category: "Networking",
+        items: ["Routing", "Subneting", "VLAN", "WLAN"],
     },
 ];
 
